10ab446454a7b3acad0f446a190d15f84b16835f
Ilya Podprugin <p0dprugin.ilia@gmail.com>
2020-10-08 03:16:36 +0300

    Create Question class with generate_message function whitch generates an inline keyboard and sends message with it

--- app.py
+++ app.py
@@ -1,111 +1,147 @@
 import telebot
 import config
 from telebot import types
-from config import TOKEN
 
 
-bot = telebot.TeleBot(TOKEN)
-
-first_question_keyboard = types.InlineKeyboardMarkup(row_width=1)
-first_question_buttons = [
-    types.InlineKeyboardButton(text="В течение недели", callback_data="В течение недели"),
-    types.InlineKeyboardButton(text="В течение месяца", callback_data="В течение месяца"),
-    types.InlineKeyboardButton(text="Через несколько месяцев", callback_data="Через несколько месяцев"),
-    types.InlineKeyboardButton(text="Точных ориепнтиров нет", callback_data="Точных ориепнтиров нет")
-]
-first_question_keyboard.add(*first_question_buttons)
-
-second_question_keyboard = types.InlineKeyboardMarkup(row_width=1)
-second_question_buttons = [
-    types.InlineKeyboardButton(text="до 40м2", callback_data="до 40м2"),
-    types.InlineKeyboardButton(text="40-60м2", callback_data="40-60м2"),
-    types.InlineKeyboardButton(text="60-85м2", callback_data="60-85м2"),
-    types.InlineKeyboardButton(text="85-120м2", callback_data="85-120м2"),
-    types.InlineKeyboardButton(text="Более 120м2", callback_data="Более 120м2"),
-    types.InlineKeyboardButton(text="Рассмотрю все варианты", callback_data="Рассмотрю все варианты"),
-    types.InlineKeyboardButton(text="Предыдущий вопрос", callback_data="previous")
-]
-second_question_keyboard.add(*second_question_buttons)
-
-third_question_keyboard = types.InlineKeyboardMarkup(row_width=1)
-third_question_buttons = [
-    types.InlineKeyboardButton(text="1-10 этаж", callback_data="1-10 этаж"),
-    types.InlineKeyboardButton(text="10-25 этаж", callback_data="10-25 этаж"),
-    types.InlineKeyboardButton(text="25-45 этаж", callback_data="25-45 этаж"),
-    types.InlineKeyboardButton(text="45-60 этаж", callback_data="45-60 этаж"),
-    types.InlineKeyboardButton(text="Выше 60 этажа", callback_data="Выше 60 этажа"),
-    types.InlineKeyboardButton(text="Этаж не важен", callback_data="Этаж не важен"),
-    types.InlineKeyboardButton(text="Предыдущий вопрос", callback_data="previous")
-]
-third_question_keyboard.add(*third_question_buttons)
-
-fourth_question_keyboard = types.InlineKeyboardMarkup(row_width=1)
-fourth_question_buttons = [
-    types.InlineKeyboardButton(text="1 месяц", callback_data="1 месяц"),
-    types.InlineKeyboardButton(text="3-6 месяцев", callback_data="3-6 месяцев"),
-    types.InlineKeyboardButton(text="6-12 месяцев", callback_data="6-12 месяцев"),
-    types.InlineKeyboardButton(text="Более 12 месяцев", callback_data="Более 12 месяцев"),
-    types.InlineKeyboardButton(text="Предыдущий вопрос", callback_data="previous")
-]
-fourth_question_keyboard.add(*fourth_question_buttons)
+bot = telebot.TeleBot(config.TOKEN)
 
 
+class Question:
+    def __init__(self, chat_id, text, answers, question_type, row_width):
+        # self.keyboard_name = keyboard_name
+        self.row_width = row_width
+        self.chat_id = chat_id
+        self.text = text
+        self.answers = answers
+        self.question_type = question_type
+
+    def generate_message(self):
+        keyboard = types.InlineKeyboardMarkup(row_width=self.row_width)
+        buttons = []
+        for i in self.answers:
+            buttons.append(types.InlineKeyboardButton(text=i, callback_data=i))
+        keyboard.add(*buttons)
+        bot.send_message(self.chat_id, self.text, parse_mode="HTML", reply_markup=keyboard)
+
+
+# @bot.message_handler(commands="start")
+# def send_message(message):
+#     first_question = QuestionGenerator(message.chat.id,
+#                                        "To be or not to be?",
+#                                        4,
+#                                        ["to be", "not to be", "be to", "be to not"],
+#                                        "select",
+#                                        1
+#                                        )
+#     first_question.generate_message()
+    # print(type(first_question.keyboard), first_question)
+    # print(first_question.text, first_question.num_of_answers, first_question.answers, first_question.question_type)
+
+# first_question_keyboard1 = types.InlineKeyboardMarkup(row_width=1)
+# first_question_buttons1 = [
+#     types.InlineKeyboardButton(text="В течение недели", callback_data="В течение недели"),
+#     types.InlineKeyboardButton(text="В течение месяца", callback_data="В течение месяца"),
+#     types.InlineKeyboardButton(text="Через несколько месяцев", callback_data="Через несколько месяцев"),
+#     types.InlineKeyboardButton(text="Точных ориепнтиров нет", callback_data="Точных ориепнтиров нет")
+# ]
+# first_question_keyboard1.add(*first_question_buttons1)
+# print(first_question_keyboard1, "first_question_keyboard1")
+#
+# second_question_keyboard = types.InlineKeyboardMarkup(row_width=1)
+# second_question_buttons = [
+#     types.InlineKeyboardButton(text="до 40м2", callback_data="до 40м2"),
+#     types.InlineKeyboardButton(text="40-60м2", callback_data="40-60м2"),
+#     types.InlineKeyboardButton(text="60-85м2", callback_data="60-85м2"),
+#     types.InlineKeyboardButton(text="85-120м2", callback_data="85-120м2"),
+#     types.InlineKeyboardButton(text="Более 120м2", callback_data="Более 120м2"),
+#     types.InlineKeyboardButton(text="Рассмотрю все варианты", callback_data="Рассмотрю все варианты"),
+#     types.InlineKeyboardButton(text="🔙 Предыдущий вопрос", callback_data="previous")
+# ]
+# second_question_keyboard.add(*second_question_buttons)
+#
+# third_question_keyboard = types.InlineKeyboardMarkup(row_width=1)
+# third_question_buttons = [
+#     types.InlineKeyboardButton(text="1-10 этаж", callback_data="1-10 этаж"),
+#     types.InlineKeyboardButton(text="10-25 этаж", callback_data="10-25 этаж"),
+#     types.InlineKeyboardButton(text="25-45 этаж", callback_data="25-45 этаж"),
+#     types.InlineKeyboardButton(text="45-60 этаж", callback_data="45-60 этаж"),
+#     types.InlineKeyboardButton(text="Выше 60 этажа", callback_data="Выше 60 этажа"),
+#     types.InlineKeyboardButton(text="Этаж не важен", callback_data="Этаж не важен"),
+#     types.InlineKeyboardButton(text="Предыдущий вопрос", callback_data="previous")
+# ]
+# third_question_keyboard.add(*third_question_buttons)
+#
+# fourth_question_keyboard = types.InlineKeyboardMarkup(row_width=1)
+# fourth_question_buttons = [
+#     types.InlineKeyboardButton(text="1 месяц", callback_data="1 месяц"),
+#     types.InlineKeyboardButton(text="3-6 месяцев", callback_data="3-6 месяцев"),
+#     types.InlineKeyboardButton(text="6-12 месяцев", callback_data="6-12 месяцев"),
+#     types.InlineKeyboardButton(text="Более 12 месяцев", callback_data="Более 12 месяцев"),
+#     types.InlineKeyboardButton(text="Предыдущий вопрос", callback_data="previous")
+# ]
+# fourth_question_keyboard.add(*fourth_question_buttons)
+
+# Сделать возможность делать кастомный callback_data
+# Осуществить это можно так:
+# В функцию в качестве параметра передаётся массив словарей, где значение - текст сообщение, а ключ - callback_data
 @bot.message_handler(commands=["start"])
 def any_msg(message):
-    greeting_keyboard = types.InlineKeyboardMarkup()
-    ready_button = types.InlineKeyboardButton(text="Пройти тест", callback_data="ready")
-    greeting_keyboard.add(ready_button)
-    bot.send_message(message.chat.id, config.greeting, reply_markup=greeting_keyboard)
+    config.chat_id = message.chat.id
+    config.user_name = f"{message.chat.first_name} {message.chat.last_name}"
+    greeting = Question(config.chat_id, config.generate_greeting(), config.first_question_answers, "select", 1)
+    first_question = Question(config.chat_id, config.generate_greeting(),
+                                       config.first_question_answers, "select", 1)
+    greeting.generate_message()
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    if call.message:
-        if call.data == "ready":
-            config.current_question = 1
-            bot.edit_message_text(
-                chat_id=call.message.chat.id,
-                message_id=call.message.message_id,
-                text=config.first_question_txt,
-                reply_markup=first_question_keyboard
-            )
-        elif call.data == "previous":
-            # config.current_question -= 1
-            bot.send_message(call.message.chat.id, "The 'Previous question' btn has been pressed")
-        elif config.current_question == 1:
-            config.first_question_answer = call.data
-            config.current_question = 2
-            bot.edit_message_text(
-                chat_id=call.message.chat.id,
-                message_id=call.message.message_id,
-                text=config.second_question_txt,
-                reply_markup=second_question_keyboard
-            )
-        elif config.current_question == 2:
-            config.second_question_answer = call.data
-            config.current_question = 3
-            bot.edit_message_text(
-                chat_id=call.message.chat.id,
-                message_id=call.message.message_id,
-                text=config.third_question_txt,
-                reply_markup=third_question_keyboard
-            )
-        elif config.current_question == 3:
-            config.third_question_answer = call.data
-            config.current_question = 4
-            bot.edit_message_text(
-                chat_id=call.message.chat.id,
-                message_id=call.message.message_id,
-                text=config.fourth_question_txt,
-                reply_markup=fourth_question_keyboard
-            )
-        else:
-            config.fourth_question_answer = call.data
-            bot.edit_message_text(
-                chat_id=call.message.chat.id,
-                message_id=call.message.message_id,
-                text=config.generate_goodbye()
-            )
+# @bot.callback_query_handler(func=lambda call: True)
+# def callback_inline(call):
+#     if call.data == "ready":
+#         config.current_question = 1
+#         bot.edit_message_text(
+#             chat_id=call.message.chat.id,
+#             message_id=call.message.message_id,
+#             text=config.first_question_txt,
+#             reply_markup=first_question_keyboard
+#         )
+#     else:
+#         if call.data == "previous":
+#             config.current_question -= 1
+#             # bot.send_message(call.message.chat.id, "The 'Previous question' btn has been pressed")
+#         elif config.current_question == 1:
+#             config.first_question_answer = call.data
+#             config.current_question = 2
+#             bot.edit_message_text(
+#                 chat_id=call.message.chat.id,
+#                 message_id=call.message.message_id,
+#                 text=config.second_question_txt,
+#                 reply_markup=second_question_keyboard
+#             )
+#         elif config.current_question == 2:
+#             config.second_question_answer = call.data
+#             config.current_question = 3
+#             bot.edit_message_text(
+#                 chat_id=call.message.chat.id,
+#                 message_id=call.message.message_id,
+#                 text=config.third_question_txt,
+#                 reply_markup=third_question_keyboard
+#             )
+#         elif config.current_question == 3:
+#             config.third_question_answer = call.data
+#             config.current_question = 4
+#             bot.edit_message_text(
+#                 chat_id=call.message.chat.id,
+#                 message_id=call.message.message_id,
+#                 text=config.fourth_question_txt,
+#                 reply_markup=fourth_question_keyboard
+#             )
+#         else:
+#             config.fourth_question_answer = call.data
+#             bot.edit_message_text(
+#                 chat_id=call.message.chat.id,
+#                 message_id=call.message.message_id,
+#                 text=config.generate_goodbye()
+#             )
 
 
 bot.polling()
--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 TOKEN = "1334996670:AAHRRRLw0lujGopuDFLFvX84gL5R7DvD5hA"
 chat_id = ""
-greeting = """Здравствуйте, я бот Арнком, Ваш личный помощник.
-Ответьте на 5 вопросов и менеджер позвонит Вам с выгодным предложением. 
-Если готовы - нажмите на кнопку.
-"""
+
+current_question = 0
+first_question_answer = ""
+second_question_answer = ""
+third_question_answer = ""
+fourth_question_answer = ""
+fifth_question_answer = ""
+first_question_answers = ["В течение недели", "В течение месяца", "Через несколько месяцев", "Точных ориепнтиров нет"]
+goodbye = ""
+user_name = ""
+
+greeting = ""
 quiz_start_phrase = "Чтож, начнём"
 first_question_txt = "Первый вопрос: Когда планируете снять офис?"
 second_question_txt = "Второй вопрос: Помещение какой площади вы рассматриваете?"
@@ -12,13 +20,14 @@ fourth_question_txt = "Четвёртый вопрос: На какой срок
 fifth_question_txt = "Пятый вопрос: Какие дополнительные параметры вам нужны?"
 something_went_wrong = "Что-то пошло не так. Чтобы начать диалог с ботом заново - отправьте комманду \"/start\""
 
-current_question = 0
-first_question_answer = ""
-second_question_answer = ""
-third_question_answer = ""
-fourth_question_answer = ""
-fifth_question_answer = ""
-goodbye = ""
+
+def generate_greeting():
+    global greeting
+    greeting = f"""<b>Здравствуйте</b>, {user_name}, я бот Арнком, Ваш личный помощник.
+Ответьте на 5 вопросов и менеджер позвонит Вам с выгодным предложением. 
+Если готовы - нажмите на кнопку.
+"""
+    return greeting
 
 
 def generate_goodbye():
